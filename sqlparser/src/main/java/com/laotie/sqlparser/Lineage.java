d3aab351d1a6ecdee390b7b742a6d44fdf098da3
zhijun <chenzhijun@fongwell.com>
2024-02-28 09:42:10 +0800

    sql解析

--- sqlparser/src/main/java/com/laotie/sqlparser/Lineage.java
+++ sqlparser/src/main/java/com/laotie/sqlparser/Lineage.java
@@ -6,29 +6,36 @@ import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.Statement;
 import net.sf.jsqlparser.statement.insert.Insert;
+import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.util.TablesNamesFinder;
 
 public class Lineage {
     public static void main(String[] args) {
         // TODO: support: insert/insert overwrite/upsert/replace/update/merge/forien key
-        String sqlStr = "insert into c1\r\n" + //
-                        "select * from(\r\n" + //
-                        "\tselect * from db1.a1 \r\n" + //
-                        "\tunion \r\n" + //
-                        "\tselect * from db1.a2\r\n" + //
-                        "\tunion \r\n" + //
-                        "\tselect * from db1.a3\r\n" + //
-                        ") aa left join db2.b1 as bb1 on a1.id = bb1.aid\r\n" + //
-                        "-- left join db2.b2 bb2 on a1.id = bb2.aid\r\n" + //
-                        "where bi.aid is not null;\r\n";
+        // String sqlStr = "-- insert into c1\r\n" + //
+        //                 "select * from(\r\n" + //
+        //                 "\tselect * from db1.a1 \r\n" + //
+        //                 "\tunion \r\n" + //
+        //                 "\tselect * from db1.a2\r\n" + //
+        //                 "\tunion \r\n" + //
+        //                 "\tselect * from db1.a3\r\n" + //
+        //                 ") aa left join db2.b1 as bb1 on a1.id = bb1.aid\r\n" + //
+        //                 "-- left join db2.b2 bb2 on a1.id = bb2.aid\r\n" + //
+        //                 "where bi.aid is not null;\r\n";
+        String sqlStr = "SELECT Column1, Column2, Column3  INTO NewTable FROM OldTable WHERE condition;";
 
         Statement statHandle;
+        Select select;
         Insert insert;
         try {
             statHandle = (Statement) CCJSqlParserUtil.parse(sqlStr);
             if (statHandle instanceof Insert){
                 insert = (Insert) statHandle;
                 parseLineage(insert);
+            }
+            if (statHandle instanceof Select){
+                select = (Select) statHandle;
+                System.out.println("well done.");
             }
             System.out.println("well done.");
         } catch (JSQLParserException e) {
